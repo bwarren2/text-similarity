7a8338e78bde08945c92b575a00d326814a144ce
Ben Warren <bwarren2@gmail.com>
2017-12-24 21:04:26 -0500

    Refactor and clean up output

--- snippets/doccheck.py
+++ snippets/doccheck.py
@@ -1,20 +1,19 @@
 import os
-import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 sources = sorted(os.listdir('../samples'))
 documents = [open("../samples/{0}".format(d)).read() for d in sources]
 
-remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+tfidf = TfidfVectorizer(
+    analyzer='word',
+    ngram_range=(1, 3),
+    min_df=0,
+    stop_words='english',
+    # tokenizer=normalize,
+).fit_transform(documents)
 
-
-def normalize(text):
-    '''remove punctuation, lowercase, stem'''
-    return text.lower().translate(remove_punctuation_map)
-
-
-tfidf = TfidfVectorizer(tokenizer=normalize).fit_transform(documents)
 pairwise_similarity = tfidf * tfidf.T
 for idx, source in enumerate(sources):
     for idx2, pair in enumerate(sources):
-            print source, pair, pairwise_similarity.A[idx][idx2]
+            if source == '110.6081.113.md':
+                print source, pair, pairwise_similarity.A[idx][idx2]
