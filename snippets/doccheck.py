ee1a5c7d9b5152dbe588862a289caa4c39659786
Ben Warren <bwarren2@gmail.com>
2017-12-24 19:36:55 -0500

    Modest refactor

--- snippets/doccheck.py
+++ snippets/doccheck.py
@@ -1,9 +1,20 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
 import os
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-documents = [open("../samples/{0}".format(d)).read() for d in os.listdir('../samples')]
+sources = sorted(os.listdir('../samples'))
+documents = [open("../samples/{0}".format(d)).read() for d in sources]
+
+remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
 
 
-tfidf = TfidfVectorizer().fit_transform(documents)
+def normalize(text):
+    '''remove punctuation, lowercase, stem'''
+    return text.lower().translate(remove_punctuation_map)
+
+
+tfidf = TfidfVectorizer(tokenizer=normalize).fit_transform(documents)
 pairwise_similarity = tfidf * tfidf.T
-print pairwise_similarity
+for idx, source in enumerate(sources):
+    for idx2, pair in enumerate(sources):
+            print source, pair, pairwise_similarity.A[idx][idx2]
